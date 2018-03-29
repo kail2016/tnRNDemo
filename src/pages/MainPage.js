ad5021edde94da74a9b9276d907d6a5f58e5ee6e
周辉2 zhouhui2 () <zhouhui2@tuniu.com>
2018-03-29 15:10:46 +0800

    [imp]promise

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -24,6 +24,75 @@ class MainPage extends Component {
         this.props.navigation.dispatch(resetAction)
     }
 
+    getNumber() {
+        var p = new Promise(function(resolve, reject){
+            //做一些异步操作
+            setTimeout(function(){
+                var num = Math.ceil(Math.random() * 10); //生成1-10的随机数
+                if(num < 5){
+                    resolve(num);
+                }else {
+                    reject('数字太大了');
+                }
+            }, 2000);
+        });
+        return p;
+    }
+
+    test(){
+
+        //----串行执行任务------
+        // runAsync1()
+        //     .then(function(data){
+        //         console.log(data);
+        //         return runAsync2();
+        //     })
+        //     .then(function(data){
+        //         console.log(data);
+        //         return runAsync3();  //这里直接返回数据
+        //     })
+        //     .then(function(data){
+        //         console.log(data);
+        //     });
+
+
+        // this.getNumber()
+        //     .then( function(data){  //第一个对应resolve的回调，第二个对应reject的回调 ;
+        //             console.log('resolved');
+        //             console.log(data);
+        //         },
+        //         function(reason, data){
+        //             console.log('rejected');
+        //             console.log(reason);
+        //         });
+
+        //getNumber()
+        //     .then(function(data){
+        //         console.log('resolved');
+        //         console.log(data);
+        //         console.log(somedata); //此处的somedata未定义
+        //     })
+        //     .catch(function(reason){
+        // 在执行resolve的回调（也就是上面then中的第一个参数）时，如果抛出异常了（代码出错了），
+        // 那么并不会报错卡死js，而是会进到这个catch方法中
+        //         console.log('rejected');
+        //         console.log(reason);
+        //     });
+
+        //三个异步操作的并行执行的，等到它们都执行完后才会进到then；返回结果按顺序存放在数组里面
+        // Promise
+        //     .all([runAsync1(), runAsync2(), runAsync3()])
+        //     .then(function(results){
+        //         console.log(results);
+        //     })
+
+        Promise
+            .race([runAsync1(), runAsync2(), runAsync3()])
+            .then(function(results){
+                console.log(results);
+            });
+    }
+
     render() {
         const {user} = this.props.navigation;
         const {count, incrementFn, decrementFn} = this.props;
@@ -33,8 +102,13 @@ class MainPage extends Component {
 
                 <TouchableOpacity onPress={this.logout.bind(this)} style={{marginTop: 50}}>
                     <View>
-                        <Text>退出登录
-                        </Text>
+                        <Text>退出登录</Text>
+                    </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={this.test.bind(this)} style={{marginTop: 20}}>
+                    <View>
+                        <Text>Promise函数测试</Text>
                     </View>
                 </TouchableOpacity>
             </View>
@@ -60,11 +134,42 @@ function mapStateToProps(state) {
 // 哪些 action 创建函数是我们想要通过 props 获取的？
 function mapDispatchToProps(dispatch) {
     return {
-        incrementFn: () => dispatch(counterAction.increment()),
+        incrementFn: () => dispatch(counterAction.addAsy()),
         decrementFn: () => dispatch(counterAction.decrement()),
     };
 }
 
+
+function runAsync1(){
+    var p = new Promise(function(resolve, reject){
+        //做一些异步操作
+        setTimeout(function(){
+            console.log('异步任务1执行完成');
+            resolve('随便什么数据1');
+        }, 1000);
+    });
+    return p;
+}
+function runAsync2(){
+    var p = new Promise(function(resolve, reject){
+        //做一些异步操作
+        setTimeout(function(){
+            console.log('异步任务2执行完成');
+            resolve('随便什么数据2');
+        }, 2000);
+    });
+    return p;
+}
+function runAsync3(){
+    var p = new Promise(function(resolve, reject){
+        //做一些异步操作
+        setTimeout(function(){
+            console.log('异步任务3执行完成');
+            resolve('随便什么数据3');
+        }, 2000);
+    });
+    return p;
+}
 
 // export  default MainPage;
 
--- src/store/ConfigureStore.js
+++ src/store/ConfigureStore.js
@@ -3,13 +3,13 @@
  */
 'use strict';
 import {applyMiddleware, createStore} from "redux";
-import thunk from "redux-thunk";
+import thunkMiddleware from "redux-thunk";
 import rootReducer from "../reducers/index";
 import {createLogger} from 'redux-logger'; // 利用redux-logger打印日志
 // import {apiMiddleware} from 'redux-api-middleware';
 
 const logger = createLogger({collapsed: true});
-const createStoreWithMiddleware = applyMiddleware(thunk,logger)(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunkMiddleware,logger)(createStore);
 
 export default function configureStore(initalState) {
     const store = createStoreWithMiddleware(rootReducer, initalState);
